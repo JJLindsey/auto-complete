dfc2a8e981e11bbf3937a4552e23e7c4b377a824
JJLindsey <jlindsey010@gmail.com>
2022-04-20 11:29:07 -0700

    add nba array & render autocomplete component

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,3 +1,24 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import { nbaTeams } from "./NbaTeams";
+
+const NbaAutocomplete = () => {
+    return (
+        <Autocomplete
+            id="nba teams"
+            options={nbaTeams}
+            renderInput={params => (
+                <TextField {...params}
+                    label="NBA Team"
+                    variant="outlined"
+                />
+            )}
+            getOptionLabel={option => option.name}
+            style={{ width: 270 }}
+            
+        />
+    )
+};
+
+export default NbaAutocomplete;
